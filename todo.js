49a0342115114e18a3b14d652f81129bfa058049
Jayant sahu <jayant3787@gmail.com>
2021-10-29 17:55:06 +0530

    List item rendered done

--- todo.js
+++ todo.js
@@ -12,25 +12,123 @@
 
 // 2 way FormDATA
 
-const todos = [{
-    title: 'meeting',
-    description : 'there is a meeting',
-    createdAt : new Date().toString(),
-    status : 'active'
-}];
+const todos = [];
 
 
 const todoForm = document.querySelector('#todoForm');
 const btn = document.querySelector('#btn');
+const title = document.querySelector('#title');
+const description = document.querySelector('#description');
+
 
 btn.addEventListener('click', function(){
     const form = new FormData(todoForm);
-    
-    for(var key of form.keys()){
-        console.log(form.get(key));
+    const formValues = {};
+    for(var val of form.keys()){
+        formValues[val] = form.get(val);
     }
 
+    var todo = {
+        title: formValues.title,
+        description: formValues.description,
+        createdAt: new Date().toString(),
+        status: 'active'
+    };
+
+    title.value = null;
+    description.value = null;
+
+    todos.push(todo);
+    render(todos);
 });
+
+function getTodo(title, description){
+    return{
+        title,
+        description,
+        createdAt: new Date().toString(),
+        status: 'active'
+    };
+
+}
+
+function render(todos){
+    const todo_list = document.querySelector('.todo_list');
+
+    const items = todos.map(todo => renderATodoItem(todo));
+    
+    // i want to clear the text inside input box after clicking the button
+    todo_list.innerHTML = null;
+
+    items.forEach(item => {
+        todo_list.appendChild(item)
+    });
+}
+
+function renderATodoItem(todo){
+    const mainRow = document.createElement('div');
+    mainRow.className = 'row jumbotron section';
+    
+    const titleDiv = document.createElement('div');
+    titleDiv.className = 'col-md-2';
+    titleDiv.textContent = todo.title;
+
+    const descriptionDiv = document.createElement('div');
+    descriptionDiv.className = 'col-md-2';
+    descriptionDiv.textContent = todo.description;
+
+    const statusDiv = document.createElement('div');
+    statusDiv.className = 'col-md-2';
+    statusDiv.textContent = todo.status;
+
+    let markCompletedDiv = document.createElement('div');
+    markCompletedDiv.className = 'col-md-2';
+    let statusBtn = document.createElement('button');
+    statusBtn.className = 'btn btn-info';
+    statusBtn.textContent = 'Mark completed';
+    markCompletedDiv.appendChild(statusBtn);
+
+    const actionDiv = document.createElement('div');
+    actionDiv.className = 'col-md-4';
+    const actionRow = document.createElement('div');
+    actionRow.className = 'row';
+    let editDiv = document.createElement('div');
+    editDiv.className = 'col-md-3';
+    statusBtn = document.createElement('button');
+    statusBtn.className = 'btn btn-success';
+    statusBtn.textContent = 'Edit';
+    editDiv.appendChild(statusBtn);
+    actionRow.appendChild(editDiv);
+
+    let deleteDiv = document.createElement('div');
+    deleteDiv.className = 'col-md-3';
+    statusBtn = document.createElement('button');
+    statusBtn.className = 'btn btn-danger';
+    statusBtn.textContent = 'Delete';
+    deleteDiv.appendChild(statusBtn);
+    actionRow.appendChild(deleteDiv);
+
+    actionDiv.appendChild(actionRow); 
+
+    console.log(mainRow);
+
+
+
+
+
+    mainRow.appendChild(titleDiv);
+    mainRow.appendChild(descriptionDiv);
+    mainRow.appendChild(statusDiv);
+    mainRow.appendChild(markCompletedDiv);
+    mainRow.appendChild(actionDiv);
+
+    return mainRow;
+
+}
+
+render(todos);
+
+
 
 // Also an another way
 
